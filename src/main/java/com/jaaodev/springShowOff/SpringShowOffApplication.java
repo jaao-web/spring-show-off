0faf4c7f3c9b8c8fe9237203a07710f4b647f186
jaao-web <joaovgarciaa@gmail.com>
2024-09-26 16:23:31 -0300

    add springboot

---
+++ src/main/java/com/jaaodev/springShowOff/SpringShowOffApplication.java
@@ -0,0 +1,13 @@
+package com.jaaodev.springShowOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShowOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShowOffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaaodev/springShowOff/SpringShowOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaaodev.springShowOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShowOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
